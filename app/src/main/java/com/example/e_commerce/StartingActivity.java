78158bd8725b08601dc91ea2f03e1930b39c021f
saiful00000 <saiful.comilla24@yahoo.com>
2020-02-13 21:56:55 +0600

    starting activity layout

--- app/src/main/java/com/example/e_commerce/StartingActivity.java
+++ app/src/main/java/com/example/e_commerce/StartingActivity.java
@@ -2,13 +2,51 @@ package com.example.e_commerce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartingActivity extends AppCompatActivity {
+
+    private Button loginButton, createAcButton, skipButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting);
+
+
+
+        // initialize fields
+        loginButton = findViewById(R.id.sign_in_btn_id);
+        createAcButton = findViewById(R.id.create_account_btn_id);
+        skipButton = findViewById(R.id.skip_btn_id);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO user login
+
+            }
+        });
+
+        createAcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO create user account
+            }
+        });
+
+        skipButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartingActivity.this, MainActivity.class));
+
+            }
+        });
+
     }
 }
