4c4bd6ec230da53bfa5d75d7dfcc9c8443035096
saiful00000 <saiful.comilla24@yahoo.com>
2020-02-16 11:36:16 +0600

    user log out

--- app/src/main/java/com/example/e_commerce/MainActivity.java
+++ app/src/main/java/com/example/e_commerce/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.e_commerce;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -18,6 +22,7 @@ import com.google.firebase.database.ValueEventListener;
 public class MainActivity extends AppCompatActivity {
 
 
+    private Toolbar toolbar;
 
     private FirebaseUser currentUser;
     private FirebaseAuth firebaseAuth;
@@ -32,6 +37,13 @@ public class MainActivity extends AppCompatActivity {
         // initialize all firebase instances
         firebaseAuth = FirebaseAuth.getInstance();
         currentUser = firebaseAuth.getCurrentUser();
+
+        // initialize views
+        toolbar = findViewById(R.id.toolbarId);
+
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("E Commerce");
 
 
         // check user existence
@@ -49,27 +61,61 @@ public class MainActivity extends AppCompatActivity {
 
     private void verifyUserExistance() {
         String currentUserId = firebaseAuth.getCurrentUser().getUid();
-        rootReference = FirebaseDatabase.getInstance().getReference();
-        rootReference.child("Users").child(currentUserId).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                // if profile information is empty then send user to update profile activity
-                if ((!dataSnapshot.child("name").getValue().equals(""))) {
-                    Toast.makeText(MainActivity.this, "Welcome!", Toast.LENGTH_SHORT).show();
-                } else {
-                    sendUserToUpdateProfileActivity();
-                }
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+        if (currentUserId.equals("dXjBngp0PkNiMedYhOU7v5mFxhK2")) {
+            // check if the user is a admin or not
+            startActivity(new Intent(MainActivity.this, AdminPannelActivity.class));
+            finish();
+            return;
+        } else {
+            rootReference = FirebaseDatabase.getInstance().getReference();
+            rootReference.child("Users").child(currentUserId)
+                    .addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            // if profile information is empty then send user to update profile activity
+                            if ((!dataSnapshot.child("name").getValue().equals(""))) {
+                                Toast.makeText(MainActivity.this, "Welcome!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                sendUserToUpdateProfileActivity();
+                            }
+                        }
 
-            }
-        });
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+        }
+
     }
 
     private void sendUserToUpdateProfileActivity() {
         startActivity(new Intent(MainActivity.this, UpdateProfileActivity.class));
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_log_out_id:
+                userSignOut();
+            case R.id.menu_setting_id:
+                //TODO setting
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void userSignOut() {
+        firebaseAuth.signOut();
+        startActivity(new Intent(MainActivity.this, StartingActivity.class));
         finish();
     }
 }
